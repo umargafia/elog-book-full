406de8f61c67222b035b8756a04b8dc4f3b0f35c
ugafia <beekankia@gmail.com>
2023-10-15 17:26:24 +0100

    finish days controllers

--- server-v2/routes/weekroute.js
+++ server-v2/routes/weekroute.js
@@ -3,11 +3,16 @@ const router = express.Router();
 
 const authController = require('./../controllers/authController');
 const weekController = require('../controllers/WeekController');
+const dayController = require('../controllers/dayController');
 
 // Protect all routes after this middleware
 router.use(authController.protect);
 router.get('/getAllWeeks/:id', weekController.getAllWeeks);
 router.get('/getWeek/:id', weekController.getWeek);
+
+router.get('/day/:weekId', dayController.GetAllDays);
+router.post('/day/create', dayController.crateDay);
+router.patch('/day/update/', dayController.updateDay);
 
 router.use(authController.restrictTo('student'));
 router.post('/create', weekController.crateWeek);
